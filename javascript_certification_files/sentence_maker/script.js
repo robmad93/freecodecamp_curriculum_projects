70fb246989769a9e76c432f582db631d3ceb0a53
Robert Madigan <131868277+robmad93@users.noreply.github.com>
2026-01-30 11:15:52 +0000

    Start javascript certification folder and add sentence maker project

---
+++ javascript_certification_files/sentence_maker/script.js
@@ -0,0 +1,19 @@
+let adjective = "fast";
+let noun = "car";
+let verb = "driving";
+let place = "town";
+let adjective2 = "careful";
+let noun2 = "road";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "tired";
+noun = "librarian";
+verb = "reading";
+place = "coffee shop";
+adjective2 = "loud";
+noun2 = "book";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
