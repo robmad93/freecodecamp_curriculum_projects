7e63da610c18f11193a1503bed59e482897065dd
Robert Madigan <131868277+robmad93@users.noreply.github.com>
2026-01-31 08:30:43 +0000

    Start fortune teller project

---
+++ javascript_certification_files/fortune_teller/script.js
@@ -0,0 +1,5 @@
+const fortune1 = "Your cat will look very cuddly today.";
+const fortune2 = "The weather will be nice tomorrow.";
+const fortune3 = "Be cautious of your new neighbors."
+const fortune4 = "You will find a new hobby soon.";
+const fortune5 = "It would be wise to avoid the color red today.";
