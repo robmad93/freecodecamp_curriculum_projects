f9568d6dcfcfa3c54d8466f5b6080f55ed777f73
Robert Madigan <131868277+robmad93@users.noreply.github.com>
2026-02-01 09:00:07 +0000

    Finish fortune teller project

--- javascript_certification_files/fortune_teller/script.js
+++ javascript_certification_files/fortune_teller/script.js
@@ -3,3 +3,26 @@ const fortune2 = "The weather will be nice tomorrow.";
 const fortune3 = "Be cautious of your new neighbors."
 const fortune4 = "You will find a new hobby soon.";
 const fortune5 = "It would be wise to avoid the color red today.";
+
+/*Code for getting a random number between 1 and 5. Another 
+method would be to use Math.ceil(Math.random() * (5 - 1) + 1) since we want to include 5 */
+let randomNumber = Math.floor(Math.random() * (6 - 1) + 1);
+let selectedFortune;
+
+if (randomNumber === 1) {
+  selectedFortune = fortune1;
+}
+else if (randomNumber === 2) {
+  selectedFortune = fortune2;
+}
+else if (randomNumber === 3) {
+  selectedFortune = fortune3;
+}
+else if (randomNumber === 4) {
+  selectedFortune = fortune4;
+}
+else {
+  selectedFortune = fortune5;
+}
+
+console.log(selectedFortune);
