79f23111acda5cf41da5de692f06c17178f449cb
Robert Madigan <131868277+robmad93@users.noreply.github.com>
2026-02-21 12:56:27 +0000

    Add user stories comments

--- javascript_certification_files/DOM_manipulation_&_events/pig_latin_translator/script.js
+++ javascript_certification_files/DOM_manipulation_&_events/pig_latin_translator/script.js
@@ -1,3 +1,14 @@
+/*
+User Stories:
+
+You should create a translatePigLatin function that accepts one string as argument.
+If the string argument begins with a consonant, your function should take the first consonant or consonant cluster, move it to the end of the word, add ay to it, and return the result.
+If the string argument begins with a vowel, your function should add way at the end and return the result.
+Your function should handle string arguments where the first vowel comes in the middle of the word and return the appropriately transformed string.
+If the string argument has no vowels, your function should add ay at the end and return the result.
+Note: For the context of this lab, vowels are a, e, i, o, and u. The letter y is not considered a vowel.
+*/
+
 function translatePigLatin(str) {
   if (/^[aeiou]/i.test(str)) {
     return str + "way";
--- javascript_certification_files/DOM_manipulation_&_events/spinal_case_converter/script.js
+++ javascript_certification_files/DOM_manipulation_&_events/spinal_case_converter/script.js
@@ -1,3 +1,11 @@
+/*
+User Stories:
+
+You should create a function named spinalCase.
+The spinalCase function should take a single argument, a string.
+The spinalCase function should return the string in spinal case format. For example, if the argument is ProductLanding page, the function should return product-landing-page.
+*/
+
 function spinalCase(str) {
   return str.replace(/([a-z])([A-Z])/g, "$1-$2").replace(/[\s_]+/g, "-").toLowerCase();
   }
